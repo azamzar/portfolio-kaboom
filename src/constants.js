6cff733a87357d4bd41ad51a1ee62d57e3579125
azamzar <azambranozarallo@gmail.com>
2024-07-30 21:38:13 +0200

    Portfolio kaboom

---
+++ src/constants.js
@@ -0,0 +1,15 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `Este es mi PC. Ultimamente he estado trabajando con JavaScript y React. Tambien he jugado a Danganronpa estos dias.
+    Y aqui esta mi <a href="https://github.com/azamzar" target="_blank">Github</a>, por si le quieres echar un vistazo. Ten paciencia si aun no tengo muchas cosas, ¡todavia estoy empezando!`,
+  "cs-degree": `Este es el titulo de mi carrera. Lo tengo en la pared porque estoy orgulloso de tenerlo, aunque no sea una rama tecnologica. Creo que me ayudo a ser quien soy hoy.`,
+  "sofa-table": `Este es mi sofa. Aqui me relajo mientras veo series. Es bastante comodo.`,
+  tv: `Esta es mi TV. Anoche estuve viendo House Of The Dragon.`,
+  bed: `Aqui es donde duermo. A veces una idea me ronda la cabeza y no puedo dormir hasta que le he dado suficientes vueltas. Una de ellas fue <a href="https://albertozambrano.com/" target="_blank">esta pagina</a> que hice para promocionarme cuando me dedicaba al marketing digital y el diseño grafico.`,
+  resume: `Este es mi escritorio. ¡Anda, deje aqui mi CV! <a href="https://github.com/azamzar/cv/blob/main/CV%20Alberto%20Zambrano.pdf" target="_blank">¿Quieres verlo?</a>
+  Si quieres, puedes contactar conmigo a traves de mi email azambranozarallo@gmail.com`,
+  projects: `Informacion sobre este portfolio: esta hecho con la biblioteca Kaboom.js, que es una biblioteca para crear juegos en JavaScript.`,
+  library: `Hay un monton de libros de todo tipo, incluso alguno sobre programacion, concretamente Java, que utilice cuando estudiaba DAW. Deberia leer el siguiente tomo de Berserk, pero eso me obligaria a releer los anteriores porque no me acuerdo.`,
+  exit: `Si quieres salir de la habitacion, simplemente cierra la ventana donde se ejecuta en tu PC (si, molaria que ahi fuera hubiese mas cosas que hacer y que esto solo fuese el inicio de un JRPG).`,
+};
